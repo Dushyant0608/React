890a4be17b4f278a6ab003d1d886619730d7fba9
Dushyant <dushyantyaduvanshi06@gmail.com>
2026-03-25 00:03:23 +0530

    Day 1 of React

---
+++ 01/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id="main">
+        <h1>Day 1</h1>
+        <h3>Starting learning react</h3>
+      </div>
+      <div id="second">
+        <h4>Learned how to create multiple divs</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
+
