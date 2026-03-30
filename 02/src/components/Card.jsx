f7560fd31cb66cf10764d3af52f92189ebc2beee
Dushyant <dushyantyaduvanshi06@gmail.com>
2026-03-30 22:24:17 +0530

    02: Component

---
+++ 02/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className='Card'>
+        <h1>Hello, This is Day 2 of React</h1>
+        <p>Today we are learning about components in react</p>
+      </div>
+    </div>
+  )
+}
+
+export default card
